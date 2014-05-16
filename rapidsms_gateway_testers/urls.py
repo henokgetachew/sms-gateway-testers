e9239858e3252155cfcfe5bebc164610331a9bb0
Henok <henygetachew@gmail.com>
2014-05-16 21:06:50 +0300

    Remove deprecated reference and fix path

--- rapidsms_gateway_testers/urls.py
+++ rapidsms_gateway_testers/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('',
                        url(r'^pushmobile-gateway-tester/$',
-                           'gateway_testers.views.pushmobile_gateway_tester',
+                           'rapidsms_gateway_testers.views.pushmobile_gateway_tester',
                            name='pushmobile_gateway_tester')
                        )
